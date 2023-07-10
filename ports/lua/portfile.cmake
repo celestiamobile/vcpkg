fe37e0eff976fada91328c21d9e1183400f1dd03
Levin Li <lilinfeng303@outlook.com>
2023-07-10 19:40:24 +0800

    Use Lua 5.1

--- ports/lua/portfile.cmake
+++ ports/lua/portfile.cmake
@@ -1,14 +1,11 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.lua.org/ftp/lua-${VERSION}.tar.gz"
-    FILENAME "lua-${VERSION}.tar.gz"
-    SHA512 d90c6903355ee1309cb0d92a8a024522ff049091a117ea21efb585b5de35776191cd67d17a65b18c2f9d374795b7c944f047576f0e3fe818d094b26f0e4845c5
+    URLS "https://www.lua.org/ftp/lua-5.1.5.tar.gz"
+    FILENAME "lua-5.1.5.tar.gz"
+    SHA512 0142fefcbd13afcd9b201403592aa60620011cc8e8559d4d2db2f92739d18186860989f48caa45830ff4f99bfc7483287fd3ff3a16d4dec928e2767ce4d542a9
 )
-vcpkg_extract_source_archive(
-    SOURCE_PATH
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
-    PATCHES
-        vs2015-impl-c99.patch
-        fix-ios-system.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
